a11301ce48df8760e0cc50056eadb4a98fa0abe9
redmanss <37474215+redmanss@users.noreply.github.com>
2023-04-24 23:27:49 +0300

    Delete witchne directory

--- witchne/js/product-page.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $(".pp-price a").on("click", function (e) {
-        let anchor = $(this);
-        $('html, body').stop().animate({scrollTop: $(anchor.attr('href')).offset().top}, 777);
-        e.preventDefault();
-        return false;
-    });
-});
