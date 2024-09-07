39d5464472eeffc77eff78560949983b5a4a937c
redmanss <37474215+redmanss@users.noreply.github.com>
2024-09-07 16:51:12 +0300

    up

--- the-miller/js/product-page.js
+++ the-miller/js/product-page.js
@@ -2,3 +2,28 @@ $(document).ready(function(){
     Fancybox.bind('[data-fancybox="pp-img"]')
     Fancybox.bind('[data-fancybox="pp-video"]')
   })
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 10,
+    navigation: {
+      nextEl: '.pp-char-next',
+      prevEl: '.pp-char-prev',
+    },
+    breakpoints: {
+      320: {
+          slidesPerView: 3
+      },
+      600: {
+          slidesPerView: 5
+      },
+      1024: {
+          slidesPerView: 7
+      },
+      1200: {
+          slidesPerView: 8
+      }
+    }
+  })
+  
