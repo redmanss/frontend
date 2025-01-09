1833924f1b74c08f4e3dd760a25871e272128896
redmanss <37474215+redmanss@users.noreply.github.com>
2025-01-09 17:58:06 +0200

    up

--- js/form-response.js
+++ js/form-response.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
     $('.close-successful-block').click(function (){
         $('.successful-block').hide()
     })
+    //
+    $("#options").change(function () {
+        if ($(this).val() === "show") {
+          $("#textarea").show();
+        } else {
+          $("#textarea").hide();
+        }
+      });
 })
+
